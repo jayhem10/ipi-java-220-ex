05bdf367bac24776c81ea5d6bcf7fb94e2948f91
jayhem10 <noble.jka@gmail.com>
2020-10-08 09:38:47 +0200

    Envoi exercice 105

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
- public class Main {
+import com.ipiecoles.java.java220.Employe;
+import org.joda.time.LocalDate;
+
+public class Main {
         public static void main(String[] args) {
             String nom;
             System.out.println("Hello World");
+
+Employe employe = new Employe();
+Employe employe1 = new Employe("John", "Doe", "M4528", LocalDate.now(), 2000d);
+
         }
     }
 
--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -1,7 +1,80 @@
 package com.ipiecoles.java.java220;
 
+import org.joda.time.LocalDate;
+
 /**
  * Created by pjvilloud on 21/09/17.
  */
 public class Employe {
+
+    private String nom;
+    private String prenom;
+    private String matricule;
+    private LocalDate dateEmbauche;
+    private Double salaire;
+
+    public Employe(){
+
+    }
+
+    public Employe(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.matricule = matricule;
+        this.dateEmbauche = dateEmbauche;
+        this.salaire = salaire;
+    }
+
+    public final Integer getNombreAnneeAnciennete(){
+         return LocalDate.now().getYear() - dateEmbauche.getYear();
+
+    }
+
+    public Integer getNbConges(){
+        return Entreprise.NB_CONGES_BASE;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public LocalDate getDateEmbauche() {
+        return dateEmbauche;
+    }
+
+    public void setDateEmbauche(LocalDate dateEmbauche) throws Exception{
+        if(dateEmbauche != null && dateEmbauche.isAfter(LocalDate.now())){
+            throw new Exception("La date d'embauche ne peut être postérieure à la date courante");
+        }
+        this.dateEmbauche = dateEmbauche;
+    }
+
+    public Double getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(Double salaire) {
+        this.salaire = salaire;
+    }
+
 }
