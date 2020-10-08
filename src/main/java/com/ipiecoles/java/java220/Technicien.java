6b6100a1de000eba1fbb25e444c566168de1e460
jayhem10 <noble.jka@gmail.com>
2020-10-08 15:11:16 +0200

    ->exercice 304

---
+++ src/main/java/com/ipiecoles/java/java220/Technicien.java
@@ -0,0 +1,39 @@
+package com.ipiecoles.java.java220;
+
+import org.joda.time.LocalDate;
+
+public class Technicien extends Employe{
+    private Integer grade;
+
+
+    public Technicien(){
+        super();
+    }
+
+    public Technicien(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Integer grade) {
+        super(nom, prenom, matricule, dateEmbauche, salaire);
+        this.grade = grade;
+    }
+
+    @Override
+    public void setSalaire(Double salaire) {
+        if (grade == null){
+            super.setSalaire(salaire);
+        }
+        super.setSalaire(salaire * (1 + grade /10d));
+    }
+
+    @Override
+    public Integer getNbConges() {
+
+        return super.getNbConges(getNbConges()+getNombreAnneeAnciennete());
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+}
